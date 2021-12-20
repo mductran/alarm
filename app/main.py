09f3cd3b960a7649d2ba87a260e36566a793cf3a
mductran <ductran032118@gmail.com>
2021-12-20 14:25:39 +0700

    add skeleton

--- app/main.py
+++ app/main.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello():
-    return '<h1>Hello, World!</h1>`'
+    return "Hello, World!\n"
+
+
+@app.route("/mode", methods=["GET", "POST"])
+def changeMode(mode=""):
+    if request.method == "GET":
+        return "current mode\n"
+    elif request.method == "POST":
+        return "changing mode\n"
+
+
+@app.route('/notification', methods=["GET", "POST"])
+def notify(message=""):
+    # get all notifications
+    if request.method == "GET":
+        return "all notifications"
+    elif request.method == "POST":
+        return "receving signal"
