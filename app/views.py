b78570da3c322548b01faf8bb6e3b46774cc904f
mductran <ductran032118@gmail.com>
2022-01-10 19:57:44 +0700

    update home.html

--- app/views.py
+++ app/views.py
@@ -1,49 +1,29 @@
 from flask import request, render_template
 from app import app
 import os
+import requests
 
 
 @app.route('/', methods=["GET", "POST"])
 def hello():
     if request.method == "GET":
+        
+        response = requests.get("http://10.247.169.41:5000")
+        print(response.json()['mode'])
+
         with app.open_resource("static/ip.txt") as f:
             ip = f.readline().decode("ascii")
-        with app.open_resource("static/mode.txt") as f:
-            mode = f.readline().decode("ascii")
         with app.open_resource("static/notification.txt") as f:
             notification = f.readline().decode("ascii")
-        return render_template("home.html", ip=ip, mode=mode, notification=notification)
+        return render_template("home.html", ip=ip, mode=response.json()['mode'], notification=notification)
 
     if request.method == "POST":
         print('request data: ', request.form)
         print('request data: {}, {}'.format(request.form['mode'], request.form['notification']))
 
-        with open(os.path.join(app.root_path, "static/mode.txt"), "r+") as f:
-            f.write(request.form["mode"])
-            f.seek(0)
-            print(f.read())
         with open(os.path.join(app.root_path, "static/notification.txt"), "r+") as f:
             f.write(request.form["notification"])
             f.seek(0)
-            print(f.read())            
+            print(f.read())
+
         return "notification received"
-
-
-# @app.route("/mode", methods=["GET", "POST"])
-# def changeMode(mode=""):
-#     if request.method == "GET":
-#         with app.open_resource("static/mode.txt") as f:
-#             for line in f:
-#                 pass
-#             mode = line
-#         return mode
-#     elif request.method == "POST":
-#         return "changing mode\n"
-
-
-# @app.route('/notification', methods=["GET", "POST"])
-# def notify(message=""):
-#     if request.method == "GET":
-#         return "all notifications", 200
-#     elif request.method == "POST":
-#         return "receving signal", 200
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 if __name__ == "__main__":
     try:
         port = int(os.environ.get('PORT', 7000))
-        # app.run(host='0.0.0.0', port=port, threaded=True, debug=True)
-        app.run(host='localhost', port=port, threaded=True, debug=True)
+        app.run('0.0.0.0')
+        # app.run(host='localhost', port=port, threaded=True, debug=True)
     except Exception as e:
         print(e)
